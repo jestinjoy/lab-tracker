320f61223b62fd7b829582279c07b2d0e44cab8f
Jestin Joy <jestinjoy@gmail.com>
2026-07-22 15:13:55 +0530

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lab-tracker/sw.js', { scope: '/lab-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
